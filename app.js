1cd9f1527083bf383cf684a681bec4699ce65254
Fausto Sacco <fsaccopadron@gmail.com>
2023-09-14 18:59:02 +0100

    added board variable to represent the states of the board

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-function addText() {
-  document.getElementById("topLeft").innerHTML = "X";
+let board = [
+  ["x", "x", "o"],
+  ["x", "x", "o"],
+  ["x", "o", "x"],
+];
+let player = "x";
+
+function addText(id) {
+  document.getElementById(id).innerHTML = player;
+  if (player == "x") {
+    player = "o";
+  } else {
+    player = "x";
+  }
+}
+
+function checkForWin() {
+  if (board[0][0] == "x" && board[0][1] == "x" && board[0][2]);
 }
