9e9c0cf41ed748e308d7ed96f854d6aacea96127
Fausto Sacco <fsaccopadron@gmail.com>
2023-09-18 10:00:13 +0100

    finished check for win function

--- app.js
+++ app.js
@@ -1,22 +1,43 @@
-let board = [
-  ["x", "x", "o"],
-  ["x", "x", "o"],
-  ["x", "o", "x"],
-];
+let board = ["", "", "", "", "", "", "", "", ""];
 let player = "x";
 
 function addText(id) {
   document.getElementById(id).innerHTML = player;
   if (player == "x") {
+    board[id] = "x";
+    checkForWin();
     player = "o";
   } else {
+    board[id] = "o";
+    checkForWin();
     player = "x";
   }
-  checkForWin();
+  console.log(board);
 }
 
 function checkForWin() {
-  if (board[0][0] == "x" && board[0][1] == "x" && board[0][2] == "x") {
-    document.getElementById("result").innerHTML = "X Wins!";
+  if (board[0] == player && board[1] == player && board[2] == player) {
+    document.getElementById("result").innerHTML = player + " Wins";
+  }
+  if (board[3] == player && board[4] == player && board[5] == player) {
+    document.getElementById("result").innerHTML = player + " Wins";
+  }
+  if (board[6] == player && board[7] == player && board[8] == player) {
+    document.getElementById("result").innerHTML = player + " Wins";
+  }
+  if (board[0] == player && board[3] == player && board[6] == player) {
+    document.getElementById("result").innerHTML = player + " Wins";
+  }
+  if (board[1] == player && board[4] == player && board[7] == player) {
+    document.getElementById("result").innerHTML = player + " Wins";
+  }
+  if (board[2] == player && board[5] == player && board[8] == player) {
+    document.getElementById("result").innerHTML = player + " Wins";
+  }
+  if (board[0] == player && board[4] == player && board[8] == player) {
+    document.getElementById("result").innerHTML = player + " Wins";
+  }
+  if (board[2] == player && board[4] == player && board[6] == player) {
+    document.getElementById("result").innerHTML = player + " Wins";
   }
 }
