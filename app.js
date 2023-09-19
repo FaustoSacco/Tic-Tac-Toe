757fb06f581f49cd1d3b2202f6bf29b55040ab85
Fausto Sacco <fsaccopadron@gmail.com>
2023-09-19 09:59:26 +0100

    stop user for clicking the same square twice

--- app.js
+++ app.js
@@ -1,43 +1,57 @@
 let board = ["", "", "", "", "", "", "", "", ""];
 let player = "x";
+let finish = false;
 
 function addText(id) {
-  document.getElementById(id).innerHTML = player;
-  if (player == "x") {
-    board[id] = "x";
-    checkForWin();
-    player = "o";
-  } else {
-    board[id] = "o";
-    checkForWin();
-    player = "x";
+  if (finish == false) {
+    if (board[id] == "") {
+      document.getElementById(id).innerHTML = player;
+      if (player == "x") {
+        board[id] = "x";
+        checkForWin();
+        player = "o";
+      } else {
+        board[id] = "o";
+        checkForWin();
+        player = "x";
+      }
+      console.log(board);
+    }
   }
-  console.log(board);
 }
 
 function checkForWin() {
   if (board[0] == player && board[1] == player && board[2] == player) {
-    document.getElementById("result").innerHTML = player + " Wins";
+    finish = true;
   }
   if (board[3] == player && board[4] == player && board[5] == player) {
-    document.getElementById("result").innerHTML = player + " Wins";
+    finish = true;
   }
   if (board[6] == player && board[7] == player && board[8] == player) {
-    document.getElementById("result").innerHTML = player + " Wins";
+    finish = true;
   }
   if (board[0] == player && board[3] == player && board[6] == player) {
-    document.getElementById("result").innerHTML = player + " Wins";
+    finish = true;
   }
   if (board[1] == player && board[4] == player && board[7] == player) {
-    document.getElementById("result").innerHTML = player + " Wins";
+    finish = true;
   }
   if (board[2] == player && board[5] == player && board[8] == player) {
-    document.getElementById("result").innerHTML = player + " Wins";
+    finish = true;
   }
   if (board[0] == player && board[4] == player && board[8] == player) {
-    document.getElementById("result").innerHTML = player + " Wins";
+    finish = true;
   }
   if (board[2] == player && board[4] == player && board[6] == player) {
+    finish = true;
+  }
+  if (finish == true) {
     document.getElementById("result").innerHTML = player + " Wins";
   }
+}
+
+function resetGame() {
+  board = ["", "", "", "", "", "", "", "", ""];
+  player = "x";
+  finish = false;
 }
